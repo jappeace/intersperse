a080dd278cc4204e919d63997c27d666409bd02b
Jappie Klooster <jappieklooster@hotmail.com>
2019-02-18 23:31:54 +0100

    Import the template
    Initial template
    
    Disable tests as they don't work for some reason
    
    Initial commit
    
    run is broken and sdist should update cabal
    
    Some more warnings
    
    Ship license
    
    Shell regeneartes default.nix before doing any cabal stuff
    
    Now detect native dependencies
    
    yes this is the whole point of using nix..
    
    Better project description
    
    mtl is better for libs
    
    Use default extensions
    
    watch more files
    
    Stylish haskell works now with default lang imports
    
    Now we can handle dependnecies, but lost cabal as build input
    
    Get rid of dependency that we can manage
    
    todo items
    
    fix sdist
    
    don't set a default github, it's wrong and should be updated on
    
    publishing
    
    Figured out how to unfree
    
    Remove pin to seperate file
    
    Update pin
    
    REMOTE BUILD FARM FRAMEWORK
    
    Also watch pin
    
    HAX for dir selection
    
    Better pin syntax, upgrade pin
    
    Add ghcid and a test suite, Add overriding shell cabalities
    
    Better readme
    
    Even better readme
    
    Add cabal install too shell
    
    Independent nix-shell for update, more robuust
    
    Add etags to ghcid
    
    Use a seperate stable hpack shell for generating default.nix
    
    This breaks the dependency upon default.nix for generating
    it in the first place.
    
    The ability to run is nice as well
    
    Add all badges so we can just remove whatever neccisary
    
    (branding by default, which is good because I'll definitly forget).
    
    Add ghci command using the cabal repl
    
    Allow easy building with travis
    
    Adding a badge
    
    Move build status away from branding
    
    Excplicitly mention file name
    
    Move badge next to the other ones
    
    Link badge to builds
    
    Add discord link
    
    Add cachix support for faster builds!
    
    Add checks for formattign
    
    Fixup hlint issues
    
    Don't depend on pin for cachix, it won't get the env variable & slow
    
    Maybe we need findutils to find stuff
    
    Explain why these shells
    
    Explain what this shell is doing
    
    Better description of what this does
    
    This should fix britanny issue
    
    Add brittany management spells
    
    Apply formatting everywehre
    
    Add autoamtic hlint refactor
    
    Bump year
    
    Add haddock hackage
    
    Add gitignoreSource
    
    Fix library tool depends
    
    Use callCabal2Nix and move nix expressions to subfolder
    
    Make logos smaller
    
    Simplify setup for testing, make ghic work reliably
    
    Give readme some more love
    
    Generate etags from hpack shell
    
    Don't need that send script anymore
    
    Haven't used this in ages
    
    Add more rigor to sdist
    
    Fix hercules CI sha
    
    Add changelog
    
    Add testing against various ghc versions for CI
    
    Reduce the amount of supported compilers
    
    Add rudementary description of how to use the template
    
    Better describe templates in git idea
    
    LInke to project example
    
    Add github actions for more lazy building
    
    Make template checklist
    
    Add github actions badge
    
    Fixup readme checklist
    
    Add bundle support
    
    Upgrade nix install blah
    
    Upgrade pin
    
    Fix ci build
    
    Add stack based ci.
    
    I can't test this locally so on the branch it goes.

---
+++ nix/pin.nix
@@ -0,0 +1,6 @@
+import (builtins.fetchGit {
+    # Descriptive name to make the store path easier to identify
+    name = "nixos-pin-19.08.2020";
+    url = https://github.com/nixos/nixpkgs/;
+    rev = "ebba57534e10f087dfd2ee0b33d1054df7437610";
+    })
---
+++ src/Lib.hs
@@ -0,0 +1,9 @@
+module Lib
+  ( libF
+  )
+where
+
+import           Control.Monad.IO.Class
+
+libF :: MonadIO m => m ()
+libF = liftIO $ putStrLn "hello, world ss"
