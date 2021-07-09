1c6c7a8e09ce4e9d6f8f46995376e6e56f972f56
Jappie Klooster <jappieklooster@hotmail.com>
2021-07-09 18:23:19 +0200

    I got the  test to compile!
    and it failed

--- test/Test/TestSpec.hs
+++ test/Test/TestSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Test.TestSpec
   ( spec
   )
@@ -6,19 +7,21 @@ where
 import           Lib
 import           Test.Hspec
 import Control.Monad.Writer.Lazy
-
-
+import Data.Functor.Identity
 
 one :: Int
 one = 1
+
+two :: Int
+two = 2
 
 spec :: Spec
 spec =
   describe "The sanity of our test setup" $ do
   it "should satisfy equality" $          one `shouldBe` 1
   it "should intersperse a list" $ do
-    let res = runIntersperse $ runWriterT $ do
-              lift $ tell [2]
-              lift $ tell [3]
-              pure 'c'
-    res `shouldBe` [1,2,1,3]
+    let (b :: [Int] , (char, w :: [Int])) = runIntersperse $ runWriterT $ do
+              tell [two]
+              tell [3]
+              lift $ pure 'c'
+    w `shouldBe` [1,2,1,3]
