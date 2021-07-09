c98cc920959548aa9244821d7ae006f037c4e2f5
Jappie Klooster <jappieklooster@hotmail.com>
2021-07-09 15:11:21 +0200

    Implement the basic cruft
    I doubt half is lawfull

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,34 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ExistentialQuantification #-}
 module Lib
-  ( libF
+  ( IntersperseT (..)
   )
 where
 
-import           Control.Monad.IO.Class
 
-libF :: MonadIO m => m ()
-libF = liftIO $ putStrLn "hello, world ss"
+-- | this allows you to interlace bind calls with a before and after callback.
+--   this is usefull to show/log progress on a computation at every side effect.
+--
+--   A good example is during the processing of some background task
+data IntersperseT m a = MkIntersperse {
+    before :: m ()
+  , underlying :: m a
+  }
+
+instance Monad m => Monad (IntersperseT m) where
+  (>>=) (MkIntersperse {before, underlying}) fun = do
+    MkIntersperse before $ do
+      before
+      a <- underlying
+      let MkIntersperse _ y = fun a
+      y
+
+instance Applicative m => Applicative (IntersperseT m) where
+  (<*>) (MkIntersperse before abF) (MkIntersperse before2 a) =
+    MkIntersperse
+      (before <* before2) -- is this unlawfull? I think this means it builds up before calls
+      (abF <*> a)
+  pure x = MkIntersperse (pure ()) (pure x)
+
+instance Functor f => Functor (IntersperseT f) where
+  fmap fun (MkIntersperse before underlying) = MkIntersperse before $ fun <$> underlying
