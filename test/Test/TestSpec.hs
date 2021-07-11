d53c15b0e39ded6916b9a0e72149b0d0113e08d4
Jappie Klooster <jappieklooster@hotmail.com>
2021-07-11 13:42:31 +0200

    Add program count test

--- test/Test/TestSpec.hs
+++ test/Test/TestSpec.hs
@@ -8,6 +8,7 @@ where
 import Lib
 import Test.Hspec
 import Control.Monad.Writer.Lazy
+import Data.IORef
 
 one :: Int
 one = 1
@@ -20,12 +21,17 @@ spec =
   describe "The sanity of our test setup" $ do
   it "should satisfy equality" $          one `shouldBe` 1
   it "should intersperse a list" $ do
-    let (_b :: [Int] , (_char, w :: [Int])) = runWriterT $ runIntersperse $  do
+    let (_b :: [Int] , (_char, w :: [Int])) = runWriterT $ runIntersperseT (tell [1]) $  do
               tell [two]
               tell [3]
               pure 'c'
     w `shouldBe` [1,2,1,3]
 
+  it "can program count" $ do
+    ref <- newIORef 0
+    runIntersperseT (liftIO $ modifyIORef ref (+1)) $ do
+      liftIO $ putStrLn "hello "
+      liftIO $ putStrLn "world"
 
-instance BeforeCall (WriterT [Int] ((,) [Int])) where
-  before = tell [1]
+    res <- readIORef ref
+    res `shouldBe` 2
