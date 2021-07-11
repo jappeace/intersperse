e205d7c3eed099060b50f8fadad2cb90595eb610
Jappie Klooster <jappieklooster@hotmail.com>
2021-07-11 13:42:53 +0200

    Revert "Try alternative api but realize I need the typeclass appraoch"
    This reverts commit 4f627c05b506a66f0f42072d31ad8a4d51dc22ed.

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,9 +6,9 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE RankNTypes #-}
 module Lib
-  ( IntersperseT
+  ( IntersperseT (..)
   , Intersperse
-  , runIntersperseT
+  , BeforeCall(..)
   )
 where
 
@@ -20,43 +20,37 @@ import Control.Monad.Writer.Lazy
 --   this is usefull to show/log progress on a computation at every side effect.
 --
 --   A good example is during the processing of some background task
-data IntersperseT m a = MkIntersperse
-  { iBefore ::  m ()
-  , iUnderlying :: m a
+data IntersperseT m a = MkIntersperse {
+  runIntersperse :: m a
   }
 
-runIntersperseT :: m () -> IntersperseT m a -> m a
-runIntersperseT before inter =
-  iUnderlying $ inter { iBefore = before }
-
+class Monad m => BeforeCall m where
+  before ::  m ()
 
 type Intersperse = IntersperseT Identity
 
-instance (Monad m) => Monad (IntersperseT m) where
-  (>>=) (MkIntersperse {iUnderlying, iBefore}) fun = do
-    MkIntersperse iBefore $ do
-      iBefore
-      a <- iUnderlying
-      let MkIntersperse _ y = fun a
+instance (BeforeCall m, Monad m) => Monad (IntersperseT m) where
+  (>>=) (MkIntersperse {runIntersperse}) fun = do
+    MkIntersperse $ do
+      before
+      a <- runIntersperse
+      let MkIntersperse y = fun a
       y
 
 instance MonadTrans IntersperseT where
-  lift = MkIntersperse (pure ())
+  lift = MkIntersperse
 
 
 instance Applicative m => Applicative (IntersperseT m) where
-  (<*>) (MkIntersperse before abF) (MkIntersperse _ a) =
-    MkIntersperse before
+  (<*>) (MkIntersperse abF) (MkIntersperse a) =
+    MkIntersperse
       (abF <*> a)
-  pure x = MkIntersperse (pure ()) (pure x)
+  pure x = MkIntersperse (pure x)
 
 instance Functor f => Functor (IntersperseT f) where
-  fmap fun (MkIntersperse before underlying) = MkIntersperse before $ fun <$> underlying
+  fmap fun (MkIntersperse underlying) = MkIntersperse $ fun <$> underlying
 
-instance (MonadWriter w m) => MonadWriter w (IntersperseT m) where
+instance (BeforeCall m, MonadWriter w m) => MonadWriter w (IntersperseT m) where
   tell = lift . tell
-  listen = lift . listen . iUnderlying
-  pass = lift . pass . iUnderlying
-
-instance MonadIO m => MonadIO (IntersperseT m) where
-  liftIO = MkIntersperse (pure ()) . liftIO
+  listen = lift . listen . runIntersperse
+  pass = lift . pass . runIntersperse
